0e738c2107279f93275cb8e780ee87704e651066
bijay.shrestha <bijay.shrestha@f1soft.com>
2021-06-21 15:57:55 +0545

    First commit.

---
+++ src/main/java/com/bijay/springbootsentryexample/SpringbootSentryExampleApplication.java
@@ -0,0 +1,13 @@
+package com.bijay.springbootsentryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSentryExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSentryExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bijay/springbootsentryexample/SpringbootSentryExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bijay.springbootsentryexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSentryExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

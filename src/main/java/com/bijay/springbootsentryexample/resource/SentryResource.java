952acfd3b09ced9aa2f3cd535605277a027f9f33
bijay.shrestha <bijay.shrestha@f1soft.com>
2021-06-21 17:06:26 +0545

    Created basic template for Sentry.

--- src/main/java/com/bijay/springbootsentryexample/resource/SentryResource.java
+++ src/main/java/com/bijay/springbootsentryexample/resource/SentryResource.java
@@ -1,15 +1,29 @@
 package com.bijay.springbootsentryexample.resource;
 
+import io.sentry.Sentry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/test/")
 public class SentryResource {
 
     @GetMapping
     public String test() {
         return "Application is running mother!";
+    }
+
+    @GetMapping("sentry")
+    public String sentry() {
+        log.info("Entering Sentry method .. ");
+        try {
+            throw new Exception("This is a test.");
+        } catch (Exception e) {
+            Sentry.captureException(e);
+        }
+        return "Babbal bhaira cha lah!";
     }
 }
